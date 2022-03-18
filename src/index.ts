5d9b3a0238426b4c1a7a8e5bea3c75e0ccb6e425
Helio <helios25@gmail.com>
2022-03-18 09:16:25 -0400

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import mjml2html from 'mjml'
+import path from 'path'
+import fs from 'fs/promises'
+
+async function getHTLMFromMJML(filePath: string) {
+  const fileContent = await fs.readFile(filePath, 'utf-8')
+  return mjml2html(fileContent.toString()).html
+}
+
+type EmailDict = {
+  name: string
+  content: string
+}
+
+async function buildEmailDict(path: string): Promise<EmailDict> {
+  const htmlContent = await getHTLMFromMJML(path)
+  const templateNameParts = path.split('/')
+  const templateName = templateNameParts.reverse()[0].split('.')[0]
+  return {name: templateName, content: htmlContent}
+}
+
+async function findEmailTemplateFiles(directory: string) {
+  const items = await fs.readdir(directory, {withFileTypes: true})
+  const fileNames = items.filter((file) => !file.isDirectory()).map((file) => `${directory}/${file.name}`)
+  const folders = items.filter((item) => item.isDirectory())
+  for (const folder of folders) {
+    fileNames.push(...await findEmailTemplateFiles(`${directory}/${folder.name}`))
+  }
+
+  return fileNames
+}
+
+type MJMLTemplatesByLocale = {
+  en: EmailDict[]
+  fr: EmailDict[]
+}
+
+async function main() {
+  const templatesDirectory = path.join(__dirname, 'templates')
+  const filePaths = await findEmailTemplateFiles(templatesDirectory)
+  const mjmlTemplatePaths = filePaths.filter((emailTemplate) => path.extname(emailTemplate).includes('mjml') && !emailTemplate.includes('partials'))
+
+  const mjmlTemplatePathsByLocale: MJMLTemplatesByLocale = {en: [], fr: []}
+
+  for (const path of mjmlTemplatePaths) {
+    if (path.includes('/en/')) {
+      mjmlTemplatePathsByLocale.en.push(await buildEmailDict(path))
+    } else if (path.includes('/fr/')) {
+      mjmlTemplatePathsByLocale.fr.push(await buildEmailDict(path))
+    }
+  }
+  const srcParentPath = path.dirname('../src')
+  const basePath = path.join(__dirname, path.basename(srcParentPath))
+
+  await fs.rm(`${basePath}/dist`, { recursive: true})
+
+  for (const key of Object.keys(mjmlTemplatePathsByLocale)) {
+    for (const templateDict of mjmlTemplatePathsByLocale[key]) {
+      const outputDirectory = `${basePath}/dist/${key}/${templateDict.name}`
+      await fs.mkdir(outputDirectory, {recursive: true})
+      const outputFilePath = `${outputDirectory}/body.html`
+      await fs.writeFile(outputFilePath, templateDict.content, 'utf-8')
+    }
+  }
+}
+
+main().then(() => {
+  console.warn('finished')
+})
---
+++ src/templates/en/password-reset/README.md
@@ -0,0 +1,16 @@
+---
+name: Password Reset
+
+---
+
+# Password Reset Template
+
+This email is used when the user asks to reset his password
+
+## Variables
+
+| Variable        | Description                                             | Type   | 
+|:----------------|:--------------------------------------------------------| :----- |
+| `link`          | The full URL to the target of the transaction.          | string | 
+| `email`         | The email of the user.                                  | string | 
+| `displayName`   | The display name of the user.                           | string | 
